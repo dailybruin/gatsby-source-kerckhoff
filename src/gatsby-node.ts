39735a25713060bd32ffd588db692ae511a4ec6e
Nathan Smith <nathan@nathansmith.io>
2018-10-14 20:15:32 -0700

    initial commit

---
+++ src/gatsby-node.ts
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+import { createHash } from 'crypto'
+
+interface Options {
+  slug: string
+}
+
+exports.sourceNodes = async (
+  { actions: { createNode }, createNodeId }: any,
+  options: Options
+) => {
+  const url = `https://kerckhoff.dailybruin.com/api/packages/flatpages/${
+    options.slug
+  }`
+  const response = await fetch(url)
+  const json = await response.json()
+  const data = json['data']
+
+  console.log(data)
+
+  Object.keys(data).forEach(key => {
+    const value = data[key]
+    createNode({
+      ...value,
+      id: createNodeId(`kerckhoff-${key}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'KerckhoffArticle',
+        content: JSON.stringify(value),
+        contentDigest: createHash('md5')
+          .update(JSON.stringify(value))
+          .digest('hex'),
+      },
+    })
+  })
+}
